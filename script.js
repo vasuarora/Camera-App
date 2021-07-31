df652e63637b1673e12daebf9aad27937d5358b3
Vasu Arora <vasuarora2112@gmail.com>
2021-08-01 02:02:41 +0530

    Recording implemented

---
+++ script.js
@@ -0,0 +1,49 @@
+let videoPlayer=document.querySelector("video");
+let recordBtn=document.querySelector("#record");
+
+let promiseToUseCamera=navigator.mediaDevices.getUserMedia({
+    audio:true,
+    video:true,
+})
+
+let chunks=[];
+let mediaRecorder;
+let isRecording=false;
+
+recordBtn.addEventListener("click",function(e){
+    if(isRecording){
+        MediaRecorder.stop();
+        isRecording=true;
+    }
+
+    else{
+        MediaRecorder.start();
+        isRecording=false;
+    }
+})
+
+promiseToUseCamera.then(function(mediaStream){
+    videoPlayer.srcObject=mediaStream;
+
+    mediaRecorder=new MediaRecorder(mediaStream);
+
+    mediaRecorder.addEventListener("dataavailable",function(e){
+        chunks.push(e.data);
+    })
+
+    mediaRecorder.addEventListener("stop",function(e){
+        let blob=new Blob(chunks, {'type' : 'video.mp4'});
+        chunks=[];
+
+        let link=URL.createObjectURL(blob);
+        let a=document.createElement("a");
+        a.href=link;
+        a.download="video.mp4";
+        a.click();
+        a.remove();
+    })
+})
+
+.catch(function(){
+    console.log("User Denied Access");
+})
